c2b50e6d103a75cae087049610c3cd11e4bf447f
Iram <cri.eiao@gmail.com>
2021-09-02 11:54:20 -0700

    ..

--- estructuras de datos/Shell Sort.py
+++ estructuras de datos/Shell Sort.py
@@ -1,43 +1,107 @@
-# Codigo para Merge Sort
+# Codigo para Shell Sort + Binary Search
 # Equipo #7: Axel Cruz 31973, Ivannia Gomez 27814, Iram Abbud 033150
 
-# Funion "random" necesaria para conseguir la lista
-import random
-import math
-
-
+# Funcion para correr Shell Sort
 def ShellSort(arr):
-  
-    # Start with a big gap, then reduce the gap
-    n = len(arr)
-    gap = math.floor(n/2)
-    print(n , ",", gap)
-    # Do a gapped insertion sort for this gap size.
-    # The first gap elements a[0..gap-1] are already in gapped 
-    # order keep adding one more element until the entire array
-    # is gap sorted
-    while gap > 0:
-  
-        for i in range(gap,n):
-  
-            # add a[i] to the elements that have been gap sorted
-            # save a[i] in temp and make a hole at position i
-            temp = arr[i]
-  
-            # shift earlier gap-sorted elements up until the correct
-            # location for a[i] is found
-            j = i
-            while  j >= gap and arr[j-gap] >temp:
-                arr[j] = arr[j-gap]
-                j -= gap
-  
-            # put temp (the original a[i]) in its correct location
-            arr[j] = temp
-        gap /= 2
+    
+    # Variable 'gap' creada para despues guardar 
+    # la distancia que se utiliza para comparar elementos 
+    gap = len(arr) + 1
+    
+    # Funcion 'while' que compara elementos
+    while gap>1:
         
-# Codigo para correr el programa e imprimir las listas desordenadas y ordenadas
-k = int(input('-'*30 + '\n# de elementos en la lista: '))
-lista = random.sample(range(1, 100), k)
-print ("-"*5 + "\nLista desordenada: \n" +  str(lista))
-ShellSort(lista)
-print('-'*5 + '\nLista ordenada:\n'+str(lista)+'\n'+'-'*30)
+        # distancia entre elementos
+        gap = gap//2
+        
+        # Condicion para que el programa corra
+        flag = True
+        while flag:
+            
+            # Inmediatamente quitamos la condicion, asumiendo 
+            # que esta corrida sera la ultima
+            flag = False
+            
+            # Variable para la posicion actual en la "lista ordenada"
+            i = 0
+            
+            # funcion while que decide si aun se debe correr la comparacion entre elementos
+            while i + gap <= len(arr)-1:
+                
+                # Se comparan 2 elementos utilizando la distancia entre ellos (gap) 
+                # y si el 1ro es mayor se cambian de posicion
+                if arr[i] > arr[i + gap]:
+                    arr[i], arr[i+gap] = arr[i+gap], arr[i]
+                    
+                    # Como hubo cambio de elementos, asumimos que esta no es la ultima 
+                    # corrida necesaria, se activa la condicion para correr el programa de nuevo
+                    flag = True
+                
+                # Se suma 1 a la variable de posicion
+                i+=1
+    return arr
+
+# Funcion para Binary Search
+def BinarySearch(arr, target):
+    
+    # Variables para poder buscar el numero pedido
+    min = 0
+    max = len(arr)-1
+    
+    # Funcion while para buscar el numero utilizando min y max
+    while True:
+        
+        # Creamos una variable con la posicion media del arreglo
+        guess = (min+max)//2
+        
+        # Si esta posicion resulta tener el numero buscado termina el programa
+        if arr[guess]==target:
+            break
+        # Si el numero en la posicion de nuestro intento es menor que el numero 
+        # buscado se suma 1 al rango minimo y se intenta otra vez
+        elif arr[guess]<target:
+            min=guess+1
+        # Si el numero de nuestro intento es mayor, se resta 1 al rango 
+        # maximo y se intenta otra vez
+        else:
+            max = guess-1
+        # Si el numero no se encuentra y se regresa un "No" 
+        # como respuesta a la funcion
+        if min>max:
+            return "No"
+    return guess
+    
+# Se le pide al ususario el # de elementos en el arreglo    
+n = int(input("ingrese el # de elementos del arreglo: "))
+
+# Funcion while que pide cuales elementos seran y checa si 
+# la cantidad coincide con los que ingreso el usuario en 'n'
+while True:
+    datos = list(map(int, input("Ingrese los numeros separandolos por un espacio: ").split()))
+    # Imprimimos un error si la condicion fue seguida
+    if len(datos)!=n:
+        print('Numero de datos incorrecto, intente otra vez!')
+    else:
+        break
+
+# Imprimimos la lista ordenada
+print(f"Lista ordenada: {ShellSort(datos)}")
+
+# Funcion while para realizar el Binary Search
+while True:
+    # Pedimos el numero que se quiere buscar
+    x = int(input("¿Que numero desea buscar? "))
+
+    # Se implementa Binary Search
+    buscar = BinarySearch(datos, x)
+    
+    # Si BinarySearch no encuentra el numero, se lo indica 
+    # al usuario y puede intentar otra vez
+    if buscar == "No":
+        print("Ese numero no existe en el arreglo")
+    
+    # Si encuentra el numero lo imprime junto con su posicion 
+    # en el arreglo y termina el programa
+    else:
+        print(f"el numero {x} se encuentra en la posicion {buscar+1} del arreglo")
+        break
