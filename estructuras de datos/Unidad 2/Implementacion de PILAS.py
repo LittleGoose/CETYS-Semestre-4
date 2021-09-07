db7c98fb218ca4455ca0660028ba6bc7e3d4c19f
Iram <cri.eiao@gmail.com>
2021-09-07 09:39:58 -0700

    codigo base para PILAS

---
+++ estructuras de datos/Unidad 2/Implementacion de PILAS.py
@@ -0,0 +1,32 @@
+# Implementacion de pilas estaticas en LISTAS
+
+# ESTRUCTURA DE PROGRMA---
+class pila():
+    def __init__(self, size):
+        self.lista = []
+        self.size = size
+        self.top = -1
+    
+    def IsEmpty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+        
+    def IsFull(self):
+        if self.size == self.top + 1:
+            return True
+        else:
+            return False
+        
+    def InsertElement(self, Item):  #Apilar
+        if not self.IsFull():
+            self.lista.append(Item)
+            self.top += 1       #+= 1 es lo mismo que self.top + 1
+            
+    def DeleteElement(self):  #Desapilar (solo el ultimo)
+        if not self.IsEmpty():
+            self.lista.pop(self.top)
+            self.top -= 1
+        
+# CODIGO DE OPERACIONES---
