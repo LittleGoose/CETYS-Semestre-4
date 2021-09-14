c72204d668f1ae534e08d4510e086baeaa7a7aa2
Iram <cri.eiao@gmail.com>
2021-09-14 15:49:01 -0700

    EXAMEN V2 GRACIAS IVANNIA

--- estructuras de datos/Examen Practico 1.py
+++ estructuras de datos/Examen Practico 1.py
@@ -68,21 +68,14 @@ def BinarySearch(arr, target):
         # Si el numero no se encuentra y se regresa un "No"
         # como respuesta a la funcion
         if min > max:
-            return "No"
+            return -1
     return guess
 
 # ----- FUNCIONES DE DATOS DE LISTA
-# Funcion que checa si cualquier numero de una lista es mayor a 10^5
-def ResInNumMAX(x):
+# Funcion que checa si cualquier numero de una lista es mayor a 10^5 o menor a 1
+def ResInNum(x):
     for i in x:
-        if i > 10**5:
-            return False
-            break
-    return True
-# Funcion que checa si cualquier numero de una lista es menor a 1
-def ResInNumMIN(x):
-    for i in x:
-        if i < 1:
+        if 1 < i > 10**5:
             return False
             break
     return True
@@ -93,8 +86,7 @@ def ResInNumMIN(x):
 while True:
     n,*datos=map(int,input('Porfavor escriba el # de elementos en su arreglo, seguido por los elementos, todo separado por 1 espacio: ').split())
     # Correr funciones para restricciones de datos individuales en la lista y guardarlos en su variable
-    MAX1 = ResInNumMAX(datos)
-    MIN1 = ResInNumMIN(datos)
+    MAX1 = ResInNum(datos)
     # Imprimimos un error con su mensaje especifico, dependiendo de la regla que no se siguio
     if len(datos) != n:
         print(f'# de elementos pedidos: {n}, # de elementos ingresados: {len(datos)}')
@@ -104,10 +96,7 @@ while True:
         print('La cantidad de datos no esta dentro de los limites (1 - 30 mil), intente otra vez!')
     elif MAX1 == False:
         print(datos)
-        print('Uno de los datos de la lista excede el limite de 100 mil, intente otra vez!')
-    elif MIN1 == False:
-        print(datos)
-        print('Uno de los datos de la lista es menor a 1, intente otra vez!')
+        print('Uno de los datos no esta dentro de los limites permitidos (1 - 100 mil), intente otra vez!')
     # Si todas las reglas fueron cumplidas, seguimos con el codigo
     else:
         break
@@ -121,37 +110,22 @@ while True:
     # Pedimos el numero que se quiere buscar
     j,*lista2=map(int,input('Porfavor escriba el # de elementos que desea buscar, seguido por los elementos, todo separado por 1 espacio: ').split())
     # Correr funciones para restricciones de datos individuales en la lista y guardarlos en su variable
-    MAX2 = ResInNumMAX(lista2)
-    MIN2 = ResInNumMIN(lista2)
+    MAX2 = ResInNum(lista2)
     # Imprimimos un error con su mensaje especifico, dependiendo de la regla que no se siguio
     if len(lista2) != j:
         print(f'# de elementos: {j}, # de elementos ingresados {len(lista2)}')
         print('Cantidad de datos ingresados diferente a # de datos especificados, intente otra vez!')
     elif MAX2 == False:
         print(lista2)
-        print('Uno de los datos que busca excede el limite de 100 mil, intente otra vez!')
-    elif MIN2 == False:
-        print(lista2)
-        print('Uno de los datos que busca es menor a 1, intente otra vez!')
+        print('Uno de los datos que busca no esta dentro de los limites (1 - 100 mil), intente otra vez!')
     # Si todas las reglas son cumplidas el programa procede
     else:
         # Se crea una lista para imprimir los resultados de Binary Search
         ListaFinal = []
         # Se implementa Binary Search con un for para poder buscar todos los numeros deseados
-        for i in range(len(lista2)):
-            # Convertimos en una variable el lugar de la lista a buscar para poder utilizarla con la funcion BinarySearch
-            BuscaBusca = lista2[i-1]       
-            
-            # Guardamos el resultado de la busqueda en una variable para utilizar en la siguiente parte del codigo
-            buscar = BinarySearch(datos, BuscaBusca)
-
-            # Si BinarySearch no encuentra el numero agrega un -1 a una lista
-            if buscar == "No":
-                ListaFinal.append(-1)
-
-            # Si encuentra el numero agrega el valor del indice del numero a una lsita
-            else:
-                ListaFinal.append(i-1)
+        for dato in lista2:
+            # Agregamos el resultado de la busqueda en la lista final de resultados de busqueda
+            ListaFinal.append(BinarySearch(datos, dato))
         break
 
 # Se imprimen las 2 lineas finales 
